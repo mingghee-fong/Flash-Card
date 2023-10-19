2e01724c00a29c479ea87aec19533f1e499cc021
Blue Pilkinton-Ching <49179161+Blue-Pilkinton-Ching@users.noreply.github.com>
2023-10-20 12:06:26 +1300

    Basic stuff

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import fsPromises from 'node:fs/promises'
+import * as Path from 'node:path'
 
 const testPage = express.Router()
 
-testPage.get('/test', (req, res) => {
-  res.send('yay')
+testPage.get('/test', async (req, res) => {
+  let json = JSON.parse(
+    await fsPromises.readFile(Path.resolve('server/data/data.json'), 'utf-8')
+  )
+
+  res.render('test', json)
 })
 
 export default testPage
