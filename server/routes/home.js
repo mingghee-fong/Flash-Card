c47124e98467b5ec0d799922fdef8199a25fe295
mingghee-fong <142976558+mingghee-fong@users.noreply.github.com>
2023-10-20 14:19:23 +1300

    Added partials, hbs files

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,14 +2,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('<h1>Welcome to Flash Cards</h1> <h2>Solidfy Your Knowledge</h2>')
-
-  // let readFile = await fsPromises.readFile(
-  //   Path.resolve('./server/data/data.json')
-  // )
-
-  // readFile = JSON.parse(readFile)
-  // res.render('home', readFile)
+  res.render('home')
 })
 
 export default router
